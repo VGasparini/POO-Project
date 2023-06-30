fba6f4debb8d1482407572c1b49dab79774f51e6
Luis Felipe Menslin <luis.fmenslinx@gmail.com>
2023-06-30 11:07:45 -0300

    surtei

--- src/main/Main.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-import main.java.Service;
-
-public class Main {
-    public static void main(String[] args) {
-        new Service();
-    }
-}
--- src/main/java/Service.java
+++
@@ -1,9 +0,0 @@
-package main.java;
-
-public class Service {
-
-    public Service() {
-        Program menu = new Program();
-        menu.mainMenu();
-    }
-}
